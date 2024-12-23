fcadf368f7a281637d7eb4c47204e54e6e10cf12
Polad <bakhip@usi.ch>
2024-12-23 14:40:33 +0100

    crypto trading software user header

---
+++ user.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cstdlib>
+#include <cmath>
+#include <unistd.h>
+
+using namespace std;
+
+class user {
+protected:
+    string username;
+    string password;
+    string susername;
+    string spassword;
+    ifstream infile;
+    ofstream outfile;
+    fstream file;
+    ofstream ofs;
+};
+
+class login_trading : public user {
+public:
+    void home();
+    void registration();
+    void login();
+    void forgot();
+    void clear_file();
+    void menu();
+    int checkbalance();
+    int checkbalance(int amo);
+    void checkcoins();
+    void withdrawal();
+    void deposit();
+    void buycoins();
+    void sellcoins();
+    void save();
+    void load();
+};
+
+class checking : public login_trading {
+public:
+    void loadings();
+    void showrandom();
+    int showquantity();
+    int showquant();
+};
